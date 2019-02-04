de41471428bb71726ceea9be50b389fb0b447d5c
Tomasz9248 <tomasz.development@gmail.com>
2019-02-04 10:38:55 +0100

    Add maven support

---
+++ src/main/java/com/tomek/controller/ListServlet.java
@@ -0,0 +1,37 @@
+package com.tomek.controller;
+
+import com.tomek.data.Animal;
+import com.tomek.data.Dog;
+import com.tomek.util.ConnectionProvider;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ListServlet extends HttpServlet {
+
+    private NamedParameterJdbcTemplate template;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Dog> animalList = getAnimals();
+
+        request.setAttribute("animalList", animalList);
+        request.getRequestDispatcher("animalList.jsp").forward(request, response);
+    }
+
+    private List<Dog> getAnimals() {
+        template = new NamedParameterJdbcTemplate(ConnectionProvider.getDSInstance());
+        String query = "SELECT Name, Race, Age, isFetching, isPurebred FROM dog";
+
+        List<Dog> animaList = template.query(query, BeanPropertyRowMapper.newInstance(Dog.class));
+        return animaList;
+    }
+}
---
+++ src/main/java/com/tomek/controller/RegisterServlet.java
@@ -0,0 +1,42 @@
+package com.tomek.controller;
+
+
+import org.apache.commons.validator.EmailValidator;
+import com.tomek.dao.DaoFactory;
+import com.tomek.dao.UserDao;
+import com.tomek.data.User;
+import com.tomek.util.DbOperationException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class RegisterServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        String user_name = request.getParameter("username");
+        String password = request.getParameter("password");
+        String email_address = request.getParameter("emailAddress");
+        try {
+            DaoFactory factory = DaoFactory.getDaoFactory(DaoFactory.MYSQL_FACTORY);
+            UserDao dao = factory.getUserDao();
+
+            User user = new User(user_name, password, email_address);
+            dao.create(user);
+
+            EmailValidator validator = EmailValidator.getInstance();
+            if (validator.isValid(email_address)) {
+                request.setAttribute("emailAddress", email_address);
+                request.setAttribute("username", user_name);
+                request.getRequestDispatcher("/EmailServlet").forward(request, response);
+            } else {
+                response.sendRedirect("register.jsp");
+            }
+        } catch (DbOperationException e) {
+            e.printStackTrace();
+            response.sendRedirect("error.jsp");
+        }
+    }
+}
